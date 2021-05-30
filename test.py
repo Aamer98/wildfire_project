dbda1a8bf3e8c943ddee390195ab1af86215e14b
Aamer98 <60957986+Aamer98@users.noreply.github.com>
2021-05-30 16:37:24 +0400

    Update test.py

--- test.py
+++ test.py
@@ -2,6 +2,11 @@ from data import get_loaders
 import albumentations as A
 from albumentations.pytorch.transforms import ToTensorV2
 from config import *
+import torch
+import torchvision
+import cv2
+import numpy as np
+import pickle
 
 
 val_transforms = A.Compose(
@@ -46,6 +51,18 @@ def save_test_predictions(
             x, f"{folder}/{idx}.png"
         )
 
+fnames = ['JORDAN_235_P1_201901281204_MGA94_55',
+       'JORDAN_294_P1_201902011150_MGA94_55',
+       'WALHALLA_313_P1_201902020733_MGA94_55',
+       'WALHALLA_353_P1_201902031625_MGA94_55',
+       'MACALISTER91_648_P1_201903070444_MGA94_55']
+
+names = {'MACALISTER91_648_P1_201903070444_MGA94_55': 'pred_1.png',
+         'JORDAN_294_P1_201902011150_MGA94_55': 'pred_3.png',
+         'JORDAN_235_P1_201901281204_MGA94_55': 'pred_4.png',
+         'WALHALLA_353_P1_201902031625_MGA94_55': 'pred_0.png',
+         'WALHALLA_313_P1_201902020733_MGA94_55': 'pred_2.png'}
+
 
 def save_test_pickle():
     for file_stem in fnames:
@@ -73,4 +90,3 @@ def save_test_pickle():
 
         with open('{}.pickle'.format(file_stem), 'wb') as handle:
             pickle.dump(mask, handle)
-
